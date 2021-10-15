3156f2fd6ffb7e3a6e8e14a64895ab58cb784b8f
Marc-Antoine Babin <marcantoinebabin@allemaglobal.com>
2021-10-15 13:34:47 -0400

    First commit

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_new_app_f5.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
